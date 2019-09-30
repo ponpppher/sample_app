8daafde9dbd631bf990280531e23f8839960715a
ponpppher <genmaier@gmail.com>
2019-10-01 02:46:42 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get '/sign_up', to: 'users#new'
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/sign_up', to: 'users#new'
+  post '/sign_up', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do
+    get sign_up_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'ul' do
+        assert_select 'li', "Password confirmation doesn't match Password"
+        assert_select 'li', "Name can't be blank"
+        assert_select 'li', "Email is invalid"
+        assert_select 'li', "Password is too short (minimum is 6 characters)"
+    end
+
+    assert_select 'div.alert-danger',"The form contains4 errors"
+    assert_select 'form[action="/sign_up"]'
+  end
+
+  test "valid signup information" do
+    get sign_up_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+end
