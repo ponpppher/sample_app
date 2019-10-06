f660b9ff2239e22002b31b2a807b4aa96e5fc0d2
ponpppher <genmaier@gmail.com>
2019-10-06 21:13:46 +0900

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    const = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+
+    BCrypt::Password.create(string, cost: const)
+  end
+
 end
